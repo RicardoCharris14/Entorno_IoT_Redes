1310984d459fd330de2c5ebbc34fcc7c6e2b109d
VicenteRamirez15 <vramirezt15@gmail.com>
2025-07-06 23:12:21 -0400

    Alerta de condiciones climaticas irregulares

--- cliente-consulta.py
+++ cliente-consulta.py
@@ -8,10 +8,28 @@ async def recolectar_datos():
                 data = await response.json()
                 for registro in data:
                     _, _, temp, pres, hum = registro
-                    if temp > 50 or hum > 80:
-                        print(f"¡ALERTA! Temperatura: {temp}, Humedad: {hum}\n")
-                    else:
-                        print(f"Valores normales: {temp}°C, {hum}% humedad\n")
+                    temperatura = (0.0 <= temp <= 5.0 or 25.0 <= temp <= 30.0)
+                    presion = (0.7 <= pres <= 0.8 or 1.2 <= pres <= 1.3)
+                    humedad = (0.0 <= hum <= 15.0 or 85.0 <= hum <= 100.0)
+                    if not temperatura and not presion and not humedad: 
+                        print(f"Condiciones normales: {temp}°C, {pres} atm y {hum}% de humedad. ")
+                    else:                        
+                        if temperatura:
+                            print(f"¡ALERTA! Temperatura: {temp}°C")
+                        else:
+                            print(f"Temperatura normal: {temp}°C")
+                        if  presion:
+                            print(f"¡ALERTA! Presion: {pres} atm")
+                        else: 
+                            print(f"Presion normal: {pres} atm")
+                        if humedad:
+                            print(f"¡ALERTA! Humedad: {hum}% \n")
+                        else: 
+                            print(f"Humedad normal: {hum}% \n")
+                        
+                    
+
+                        
             
             await asyncio.sleep(5)
     
